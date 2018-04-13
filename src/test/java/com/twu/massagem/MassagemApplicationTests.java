3c91b60cbbe73db2d546a41a3de61367a187aeac
Rainara <rainaramateus@gmail.com>
2018-04-13 14:11:21 -0300

    Initial commit

---
+++ src/test/java/com/twu/massagem/MassagemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.twu.massagem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MassagemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
