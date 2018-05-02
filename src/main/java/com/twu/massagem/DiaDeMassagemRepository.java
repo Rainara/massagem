32171a8c1fa4fd8581be70fafa8127cf64ae563e
Rainara <rainaramateus@gmail.com>
2018-05-02 15:51:01 -0300

    Testes de verificacao de dados de Entrada - Quantidade/datas/Tipo

--- src/main/java/com/twu/massagem/DiaDeMassagemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.twu.massagem;
-import com.twu.massagem.DiaDeMassagem;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DiaDeMassagemRepository extends CrudRepository<DiaDeMassagem, Long>{
-}
