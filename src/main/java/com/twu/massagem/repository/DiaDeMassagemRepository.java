51f5c3063a82d2f08801523dfa45ed351ae33bfc
Rainara <rainaramateus@gmail.com>
2018-05-07 14:16:56 -0300

    Criacao HTML do adicionar dia de massagem

--- src/main/java/com/twu/massagem/repository/DiaDeMassagemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.twu.massagem.repository;
-
-import com.twu.massagem.model.DiaDeMassagem;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DiaDeMassagemRepository extends CrudRepository<DiaDeMassagem, Long>{
-}
